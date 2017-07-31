1286c71513de8e4b87b9b68ffc36242014b229e6
Larson <rlrson@uiowa.edu>
2017-07-31 16:53:05 -0500

    Add temp MOOC javascript.

---
+++ src/mooc.js
@@ -0,0 +1,14 @@
+var mooc_courses = {
+  // 2017
+  'Power of the Pen: Identities and Social Issues in Fiction and Nonfiction': 'https://iwp.uiowa.edu/page/power-of-the-pen-2017-moocs',
+  'Power of the Pen: Identities and Social Issues in Poetry and Plays':       'https://iwp.uiowa.edu/page/power-of-the-pen-2017-moocs',
+  // 2016
+  'How Writers Write Fiction 2016: Storied Women':                            'https://iwp.uiowa.edu/iwp-courses/distance-learning-courses/fall-2016/fiction-2016',
+  'Whitman’s Civil War: Writing and Imaging Death, Loss, and Disaster':       'http://iwp.uiowa.edu/whitman-2016',
+  '#Flashwrite Teen Poetry MOOC':                                             'http://iwp.uiowa.edu/flashwrite',
+  // 2015
+  'How Writers Write Fiction 2015':                                           'http://iwp.uiowa.edu/iwp-courses/distance-learning-courses/fall-2015/how-writers-write-fiction',
+  'Nonfiction Writing Seminar with Elena Passarello':                         'http://iwp.uiowa.edu/iwp-courses/distance-learning-courses/upcoming-courses/nonfiction-seminar-passarello',
+  'Nonfiction Writing Seminar with Amy Leach':                                'http://iwp.uiowa.edu/iwp-courses/distance-learning-courses/upcoming-courses/nonfiction-seminar-leach',
+  'How Writers Write Poetry 2015':                                             ''
+};
