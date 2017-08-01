54ed19339daaa6f3de56bf109e095fdca4e62596
Larson <rlrson@uiowa.edu>
2017-08-01 17:29:19 -0500

    Tenatively creating the MOOC map. Needs a lot of data munging still within the application!

--- src/mooc.js
+++ src/mooc.js
@@ -1,11 +1,20 @@
-var mooc_courses = {
+var lookup_page = {
+  "poetry-and-plays-2017": 'Power of the Pen: Identities and Social Issues in Poetry and Plays',
+  "fiction-and-nonfiction-2017": 'Power of the Pen: Identities and Social Issues in Fiction and Nonfiction',
+  "how-writers-write-fiction-2016": 'How Writers Write Fiction 2016: Storied Women',
+  "flash-write-2016": '#Flashwrite Teen Poetry',
+  "how-writers-write-fiction-2015": 'How Writers Write Fiction 2015',
+  "whitman-2016": 'Whitman’s Civil War: Writing and Imaging Death, Loss, and Disaster'
+};
+
+var lookup_link = {
   // 2017
   'Power of the Pen: Identities and Social Issues in Fiction and Nonfiction': 'https://iwp.uiowa.edu/page/power-of-the-pen-2017-moocs',
   'Power of the Pen: Identities and Social Issues in Poetry and Plays':       'https://iwp.uiowa.edu/page/power-of-the-pen-2017-moocs',
   // 2016
   'How Writers Write Fiction 2016: Storied Women':                            'https://iwp.uiowa.edu/iwp-courses/distance-learning-courses/fall-2016/fiction-2016',
   'Whitman’s Civil War: Writing and Imaging Death, Loss, and Disaster':       'http://iwp.uiowa.edu/whitman-2016',
-  '#Flashwrite Teen Poetry MOOC':                                             'http://iwp.uiowa.edu/flashwrite',
+  '#Flashwrite Teen Poetry':                                                  'http://iwp.uiowa.edu/flashwrite',
   // 2015
   'How Writers Write Fiction 2015':                                           'http://iwp.uiowa.edu/iwp-courses/distance-learning-courses/fall-2015/how-writers-write-fiction',
   'Nonfiction Writing Seminar with Elena Passarello':                         'http://iwp.uiowa.edu/iwp-courses/distance-learning-courses/upcoming-courses/nonfiction-seminar-passarello',
